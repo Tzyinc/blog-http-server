fb2bc38051ff9a81f27b3a99aecc547ee13c2c20
Zhi Yang Ten <tenzy@seagroup.com>
2019-12-04 16:39:47 +0800

    !added qr code scanner;

---
+++ qrScanner/index.js
@@ -0,0 +1,36 @@
+
+let groupId;
+let member;
+let members;
+
+async function app() {
+    let scanner = new Instascan.Scanner({ video: document.getElementById('webcam') });
+    scanner.addListener('scan', function (content) {
+        if (validURL(content)) {
+            document.getElementById('qrInfo').innerHTML = `<a href="${content}">${content}</a>`;
+        } else {
+            document.getElementById('qrInfo').innerText = content;
+        }
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
+}
+
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
+}
+
+app();
