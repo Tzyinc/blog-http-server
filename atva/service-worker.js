41ec67769135beb12b5a5abe4f6584a3fe321142
Zhi Yang Ten <tenzy@seagroup.com>
2020-01-22 23:09:41 +0800

    !test

--- atva/service-worker.js
+++ atva/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.396da55e896633e2a1a0bb1d6299e00f.js"
+  "/atva/precache-manifest.396da55e896633e2a1a0bb1d6299e00f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/atva/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
