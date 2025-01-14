489ea504c17ae8040847d5e2cf94c290552e1236
Tzyinc <tzyinc@hotmail.com>
2025-01-14 20:45:33 +0800

    add visuals

--- Metronomical/index.js
+++ Metronomical/index.js
@@ -80,12 +80,12 @@ class WorkerMetronome extends BaseMetronome {
 
     this.worker.onmessage = (props) => {
       const { message, interval } = props.data;
-      console.log(
-        "received",
-        props.data,
-        this.scheduled,
-        this.audioCtx.currentTime
-      );
+      // console.log(
+      //   "received",
+      //   props.data,
+      //   this.scheduled,
+      //   this.audioCtx.currentTime
+      // );
       if (message === "tick") {
         const gap = interval / 1000;
         const len = this.scheduled.length;
@@ -99,16 +99,11 @@ class WorkerMetronome extends BaseMetronome {
       } else if (message === "stop") {
         this.scheduled = [];
       }
-      //housekeeping
-      this.scheduled = this.scheduled.filter(
-        (item) => item > this.audioCtx.currentTime
-      );
     };
   }
 
   changeTempo(newTempo) {
     this.tempo = newTempo;
-
     const timeoutDuration = (60 / this.tempo) * 1000;
     this.worker.postMessage({
       message: "set_interval",
@@ -119,6 +114,15 @@ class WorkerMetronome extends BaseMetronome {
   stop() {
     super.stop();
     this.worker.postMessage({ message: "stop" });
+  }
+
+  cleanScheduled() {
+    const before = this.scheduled.length;
+    this.scheduled = this.scheduled.filter(
+      (item) => item > this.audioCtx.currentTime
+    );
+
+    return Boolean(this.scheduled.length - before);
   }
 }
 
@@ -138,7 +142,9 @@ startBtn.addEventListener("click", handleStart);
 tempoButtons.forEach((button) => {
   button.addEventListener("click", handleTempo);
 });
+
 let metronome;
+
 function handleStart(e) {
   if (!metronome) {
     metronome = new WorkerMetronome(120);
@@ -160,3 +166,41 @@ function handleTempo(e) {
   metronome.changeTempo(Number(e.target.id));
   console.log(e.target.id);
 }
+
+function timestamp() {
+  return window.performance && window.performance.now
+    ? window.performance.now()
+    : new Date().getTime();
+}
+
+let dt;
+let last = timestamp();
+let FPS = 120;
+let fpsCount = [];
+
+function loop() {
+  let now = timestamp();
+  dt = dt + Math.min(1, (now - last) / 1000);
+  while (dt > FPS) {
+    dt = dt - FPS;
+  }
+  render(dt, now);
+  last = now;
+  requestAnimationFrame(loop);
+}
+
+const fpsDiv = document.getElementById("fps");
+
+const EPSILON = 0.01;
+
+function render(dt, now) {
+  fpsCount = fpsCount.filter((time) => now - time < 1000);
+  const shouldRed = metronome?.cleanScheduled();
+  fpsDiv.innerHTML = `
+    fps: ${fpsCount.length}<br/>
+    ${shouldRed ? "🔴" : "⬜️"}
+  `;
+  fpsCount.push(timestamp());
+}
+
+requestAnimationFrame(loop);
